98ff4626a334eb199672abbe6acab86a6346bdfe
rmmcosta <ricardocosta101085@gmail.com>
2019-09-11 15:11:00 +0100

    added flag SYSTEM_UI_FLAG_VISIBLE to the hide function

--- src/android/NavigationBar.java
+++ src/android/NavigationBar.java
@@ -170,7 +170,8 @@ public class NavigationBar extends CordovaPlugin {
 		| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 		//| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 		//| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-		| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+		| View.SYSTEM_UI_FLAG_VISIBLE;
 		Activity activity=cordova.getActivity();
 		activity.getWindow().getDecorView().setSystemUiVisibility(newVis);	
 	}
