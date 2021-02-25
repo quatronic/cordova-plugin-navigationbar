147520235ceac7bd5dc156bac6f9d24ce1fc6b41
robvandenbroek <34415226+robvandenbroek@users.noreply.github.com>
2021-02-25 12:06:11 +0100

    Update NavigationBar.java

--- src/android/NavigationBar.java
+++ src/android/NavigationBar.java
@@ -219,7 +219,7 @@ public class NavigationBar extends CordovaPlugin {
 	private void _hideNavigationBar(boolean enableImmersiveSticky, boolean hideStatusBar) {
 		Activity activity = cordova.getActivity();
 		View decorView = activity.getWindow().getDecorView();
-		int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+		int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 		if (hideStatusBar) {
 			uiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
 		}
